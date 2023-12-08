f3ec3b712d5c0cd1482c42785d7a3d967916efe7
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-12-08 21:03:28 +0530

    Some graphql schemas

--- Sources/App/GraphQL/Resolver.swift
+++ Sources/App/GraphQL/Resolver.swift
@@ -6,13 +6,37 @@
 //
 
 import Graphiti
+import Fluent
 import Vapor
 
+struct GetRegisteredUserArgs: Codable {
+    let id: String
+}
+
+struct GetUserArgs: Codable {
+    let id: String
+    let email: String
+}
+
 final class Resolver {
-  func getAllUsers(
-    request: Request,
-    arguments: NoArguments
-  ) throws -> EventLoopFuture<[User]> {
-    User.query(on: request.db).all()
-  }
+    func getAllUsers(request: Request, arguments: NoArguments) throws -> EventLoopFuture<[User]> {
+        User.query(on: request.db).all()
+    }
+    
+    func getAllRegisteredUsers(request: Request, arguments: NoArguments) throws -> EventLoopFuture<[RegisteredUser]> {
+        RegisteredUser.query(on: request.db).all()
+    }
+    
+    func getUser(request: Request, arguments: GetUserArgs) throws -> EventLoopFuture<User> {
+        User.query(on: request.db)
+            .filter(\.$id == arguments.id)
+            .filter(\.$email == arguments.email)
+            .first().unwrap(or: Abort(.notFound))
+    }
+    
+    func getRegisteredUser(request: Request, arguments: GetRegisteredUserArgs) throws -> EventLoopFuture<RegisteredUser> {
+        RegisteredUser.query(on: request.db)
+            .filter(\.$id == arguments.id)
+            .first().unwrap(or: Abort(.notFound))
+    }
 }
