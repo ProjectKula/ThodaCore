84f7f60c32c4482cb89f7d5b575beadd5911f1a0
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-01-02 13:33:11 +0530

    it compiles

--- Sources/App/GraphQL/Resolver.swift
+++ Sources/App/GraphQL/Resolver.swift
@@ -17,18 +17,18 @@ final class Resolver {
     static let instance: Resolver = .init()
     
     func getAllUsers(request: Request, arguments: NoArguments) async throws -> [UnregisteredUser] {
-        assertPermission(request: request, .query)
-        try await UnregisteredUser.query(on: request.db).all()
+        try await assertPermission(request: request, .query)
+        return try await UnregisteredUser.query(on: request.db).all()
     }
     
     func getAllRegisteredUsers(request: Request, arguments: NoArguments) async throws -> [RegisteredUser] {
-        assertPermission(request: request, .query)
-        try await RegisteredUser.query(on: request.db).all()
+        try await assertPermission(request: request, .query)
+        return try await RegisteredUser.query(on: request.db).all()
     }
     
     func getUser(request: Request, arguments: GetUserArgs) async throws -> UnregisteredUser {
-        assertPermission(request: request, .query)
-        try await UnregisteredUser.query(on: request.db)
+        try await assertPermission(request: request, .query)
+        return try await UnregisteredUser.query(on: request.db)
             .filter(\.$id == arguments.id)
             .filter(\.$email == arguments.email)
             .first()
@@ -37,7 +37,7 @@ final class Resolver {
     }
     
     func getRegisteredUser(request: Request, arguments: GetRegisteredUserArgs) async throws -> RegisteredUser {
-        try await RegisteredUser.query(on: request.db)
+        return try await RegisteredUser.query(on: request.db)
             .filter(\.$regNo == arguments.regNo)
             .first()
             .unwrap(or: Abort(.notFound))
