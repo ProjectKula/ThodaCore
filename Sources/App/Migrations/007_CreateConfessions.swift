bd55f60aa6a9917f60c243ada791544045612bb8
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-02-19 20:41:32 +0530

    Confessions schema

---
+++ Sources/App/Migrations/007_CreateConfessions.swift
@@ -0,0 +1,24 @@
+//
+//  007_CreateConfessions.swift
+//
+//
+//  Created by Shrish Deshpande on 19/02/24.
+//
+
+import Fluent
+
+struct CreateConfessions: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        return database.schema("confessions")
+            .field("id", .int, .custom("GENERATED ALWAYS AS IDENTITY"))
+            .field("creator", .int, .required, .references("registeredUsers", "id"))
+            .field("content", .string, .required)
+            .field("date_created", .datetime, .required)
+            .unique(on: "id")
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        return database.schema("confessions").delete()
+    }
+}
