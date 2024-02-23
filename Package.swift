542e79b025c41a990b2c41d7cea994ff7bd93445
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-02-23 23:49:05 +0530

    Builds on linux

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         .package(url: "https://github.com/alexsteinerde/graphql-kit.git", branch: "master"),
         .package(url: "https://github.com/Mikroservices/Smtp.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/redis.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/redis.git", from: "4.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")        
     ],
     targets: [
         .executableTarget(
@@ -25,7 +26,8 @@ let package = Package(
                 .product(name: "GraphQLKit", package: "graphql-kit"),
                 .product(name: "Smtp", package: "Smtp"),
                 .product(name: "JWT", package: "jwt"),
-                .product(name: "Redis", package: "redis")
+                .product(name: "Redis", package: "redis"),
+                .product(name: "Logging", package: "swift-log")
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
