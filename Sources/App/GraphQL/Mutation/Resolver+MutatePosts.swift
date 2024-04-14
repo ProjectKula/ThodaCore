70fab67d90d3a7031be810f1d1d1dd4877823738
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-04-14 19:16:15 +0530

    Post replying graphql

--- Sources/App/GraphQL/Mutation/Resolver+MutatePosts.swift
+++ Sources/App/GraphQL/Mutation/Resolver+MutatePosts.swift
@@ -13,11 +13,17 @@ struct CreatePostArgs: Codable {
     var content: String
 }
 
+struct ReplyToPostArgs: Codable {
+    var to: String
+    var content: String
+}
+
 struct LikePostArgs: Codable {
     var post: String
 }
 
-extension Resolver {    
+extension Resolver {
+    // MARK: Creating posts
     func createPost(request: Request, arguments: CreatePostArgs) async throws -> Post {
         let token = try await getAndVerifyAccessToken(req: request)
         try await assertScope(request: request, .createPosts)
@@ -25,12 +31,24 @@ extension Resolver {
         try await post.create(on: request.db)
         return post
     }
-    
+
+    func replyToPost(request: Request, arguments: ReplyToPostArgs) async throws -> Post {
+        let token = try await getAndVerifyAccessToken(req: request)
+        try await assertScope(request: request, .createPosts)
+        let post = try await Post.query(on: request.db).filter(\.$id == arguments.to).first()
+          .unwrap(orError: Abort(.notFound, reason: "Could not find post with given ID")).get()
+        let reply = Post(userId: token.id, content: arguments.content)
+        reply.$reply.id = post.id
+        try await reply.create(on: request.db)
+        return reply
+    }
+
+    // MARK: Deleting posts
     func archivePost(request: Request, arguments: StringIdArgs) async throws -> Post {
         let token = try await getAndVerifyAccessToken(req: request)
         try await assertScope(request: request, .deletePosts)
         let post = try await Post.query(on: request.db).filter(\.$id == arguments.id).first()
-            .unwrap(orError: Abort(.notFound, reason: "Could not find post with given ID")).get()
+          .unwrap(orError: Abort(.notFound, reason: "Could not find post with given ID")).get()
         
         if token.id != post.$creator.id {
             request.logger.error("User \(token.id) tried to archive a post by \(post.$creator.id)")
@@ -70,7 +88,8 @@ extension Resolver {
         try await post.restore(on: request.db)
         return post
     }
-    
+
+    // MARK: Liking posts
     func likePost(request: Request, arguments: StringIdArgs) async throws -> Int {
         try await assertScope(request: request, .createPosts)
         let token = try await getAndVerifyAccessToken(req: request)
