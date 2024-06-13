8ba0ed3b6a3d337aef22bf113ac9e278cc967b6f
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-13 19:17:03 +0530

    Controller to verify

--- Sources/App/Controllers/Auth/LoginController.swift
+++ Sources/App/Controllers/Auth/LoginController.swift
@@ -11,23 +11,23 @@ import Fluent
 struct LoginController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let e = routes.grouped("v0").grouped("auth").grouped("login")
-        
+
         e.post("email", use: authWithEmail)
         e.get("email", use: methodNotAllowed)
-        
+
         e.post("id", use: authWithId)
         e.get("id", use: methodNotAllowed)
     }
-    
+
     func authWithEmail(req: Request) async throws -> AuthResponseBody {
         let args: EmailLoginArgs
-        
+
         do {
             args = try req.content.decode(EmailLoginArgs.self)
         } catch {
             throw Abort(.badRequest, reason: "Invalid request: \(error.localizedDescription)")
         }
-        
+
         return try await UserPassword.query(on: req.db)
             .join(RegisteredUser.self, on: \UserPassword.$id == \RegisteredUser.$id)
             .filter(RegisteredUser.self, \.$email == args.email)
@@ -36,16 +36,16 @@ struct LoginController: RouteCollection {
             .get()
             .auth(req: req, args: args)
     }
-    
+
     func authWithId(req: Request) async throws -> AuthResponseBody {
         let args: CollegeIdLoginArgs
-        
+
         do {
             args = try req.content.decode(CollegeIdLoginArgs.self)
         } catch {
             throw Abort(.badRequest, reason: "Invalid request: \(error.localizedDescription)")
         }
-        
+
         return try await UserPassword.query(on: req.db)
             .join(RegisteredUser.self, on: \UserPassword.$id == \RegisteredUser.$id)
             .filter(RegisteredUser.self, \RegisteredUser.$collegeId.$id == args.collegeId)
@@ -54,7 +54,7 @@ struct LoginController: RouteCollection {
             .get()
             .auth(req: req, args: args)
     }
-    
+
     @inlinable
     func methodNotAllowed(req: Request) async throws -> AuthResponseBody {
         throw Abort(.methodNotAllowed)
