29adb023cfd8d3698f6c1f57961704a6f114f624
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-07-03 21:37:25 +0530

    Use dataloaders

--- Sources/App/GraphQL/Relation/Notification+Resolver.swift
+++ Sources/App/GraphQL/Relation/Notification+Resolver.swift
@@ -13,9 +13,12 @@ extension Notification {
     func getTargetUser(request: Request, arguments: NoArguments) async throws -> RegisteredUser {
         return try await self.$targetUser.get(on: request.db)
     }
-
+    
     func getReferenceUser(request: Request, arguments: NoArguments) async throws -> RegisteredUser? {
-        return try await self.$referenceUser.get(on: request.db)
+        guard let refId = self.$referenceUser.id else {
+            return nil
+        }
+        return try await request.loaders.users.load(key: refId, on: request.eventLoop)
     }
 
     func getReferencePost(request: Request, arguments: NoArguments) async throws -> Post? {
